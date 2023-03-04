fc8da35ca4b4249a855147064fdc33db4240bbeb
qingyang <zhang.qingyang@outlook.com>
2023-03-03 16:27:22 -0800

    add create flag

---
+++ fastrag.py
@@ -0,0 +1,54 @@
+import os
+import sys
+import argparse
+import logging
+from pathlib import Path
+import random
+
+from fastrag.stores import PLAIDDocumentStore
+from fastrag.retrievers.colbert import ColBERTRetriever
+
+def main():
+    parser = argparse.ArgumentParser("Create an index using PLAID engine as a backend")
+    parser.add_argument("--gpus", type=int, default=0)
+    parser.add_argument("--ranks", type=int, default=1)
+    parser.add_argument("--index", type=int, default=0)
+    args = parser.parse_args()
+    
+    dataroot = 'data'
+    dataset = 'effective'
+    datasplit = 'train'
+
+    queries = os.path.join(dataroot, dataset, datasplit, 'questions.search.tsv')
+    collection = os.path.join(dataroot, dataset, datasplit, 'collection.tsv')
+
+    nbits = 2
+    create = True if args.index else False
+    index_name = f'{dataset}.{datasplit}.{nbits}bits'
+    
+    store = PLAIDDocumentStore(
+        index_path=index_name,
+        checkpoint_path="Intel/ColBERT-NQ",
+        collection_path=collection,
+        create=create,
+        nbits=nbits,
+        gpus=args.gpus,
+        ranks=args.ranks,
+        doc_maxlen=120,
+        query_maxlen=60,
+        kmeans_niters=4,
+    )
+
+    sampled_q = queries[random.randint(0, len(queries))]
+    print(sampled_q)
+
+    print('****** Query!')
+    print(store.query(sampled_q,top_k = 3))
+
+    print('****** Retrieve!')
+    retriever = ColBERTRetriever(store)
+    print(retriever.retrieve(sampled_q, 3))
+
+
+if __name__ == "__main__":
+    main()
