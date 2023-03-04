1095fd235d09448b6a284b186c85f490b2381cbc
qingyang <zhang.qingyang@outlook.com>
2023-03-03 18:16:43 -0800

    pipeline

--- fastrag.py
+++ fastrag.py
@@ -3,11 +3,15 @@ import sys
 import argparse
 import logging
 from pathlib import Path
-import random
 import pandas as pd
+import json
 
 from fastrag.stores import PLAIDDocumentStore
 from fastrag.retrievers.colbert import ColBERTRetriever
+
+from haystack import Pipeline
+
+
 
 def main():
     parser = argparse.ArgumentParser("Create an index using PLAID engine as a backend")
@@ -22,6 +26,8 @@ def main():
 
     queries_path = os.path.join(dataroot, dataset, datasplit, 'questions.search.tsv')
     collection_path = os.path.join(dataroot, dataset, datasplit, 'collection.tsv')
+
+    queries = pd.read_csv(queries_path, sep='\t', header=None)
 
     nbits = 2
     create = True if args.index else False
@@ -40,17 +46,26 @@ def main():
         kmeans_niters=4,
     )
     
-    queries = pd.read_csv(queries_path, sep='\t', header=None)
-    sampled_q = queries[1][random.randint(0, len(queries))]
-    print(sampled_q)
-
-    print('****** Retrieve!')
+    p = Pipeline()
     retriever = ColBERTRetriever(store)
-    res = retriever.retrieve(sampled_q, 3)
-   
-    for r in res:
-        print(r)
+    p.add_node(component=retriever, name="Retriever", inputs=["Query"])
 
+    res = dict()
+    for i in range(len(queries)):
+        q = queries[1][i]
+        d = p.run(query=q)
+        res[q] = d["documents"][:3]
+    
+    with open("data/effective/res.json", "w") as outfile:
+        json.dump(res, outfile)
+   
+
+    #from haystack.nodes import BM25Retriever, SentenceTransformersRanker
+    #from fastrag.readers import T5Reader
+
+    #retriever = BM25Retriever(document_store=document_store, top_k=100)
+    #reranker = SentenceTransformersRanker(model_name_or_path="cross-encoder/ms-marco-MiniLM-L-12-v2", top_k=10)
+    #reader = T5Reader(model_name_or_path="google/flan-t5-base", input_converter_mode="summarization", input_converter_tokenizer_max_len=16300,  min_length=10, max_length=100, num_beams=4, top_k=1, use_gpu=False)
 
 
 if __name__ == "__main__":
