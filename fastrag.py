7e88fcdcb4aa2acd8a55dd366b9525bebc8ef50f
qingyang <zhang.qingyang@outlook.com>
2023-03-04 21:12:25 -0800

    retrieve only

--- fastrag.py
+++ fastrag.py
@@ -11,7 +11,7 @@ from haystack.nodes import SentenceTransformersRanker
 from fastrag.readers import T5Reader
 from haystack import Pipeline
 
-def jsonify(res, reader=True, k=1, j=3):
+def jsonify(res, reader=1, k=1, j=3):
 
     output = {
         'query': res['query'],
@@ -45,6 +45,7 @@ def main():
     parser.add_argument("--gpus", type=int, default=0)
     parser.add_argument("--ranks", type=int, default=1)
     parser.add_argument("--index", type=int, default=0)
+    parser.add_argument("--generative", type=int, default=1)
     args = parser.parse_args()
     
     dataroot = 'data'
@@ -76,7 +77,7 @@ def main():
     p = Pipeline()
     retriever = ColBERTRetriever(
         store, 
-        top_k=100
+        top_k=100 if args.generative else 10
     )
     reranker = SentenceTransformersRanker(
         model_name_or_path="cross-encoder/ms-marco-MiniLM-L-12-v2", 
@@ -92,19 +93,19 @@ def main():
         use_gpu=False
     )
     p.add_node(component=retriever, name="Retriever", inputs=["Query"])
-    p.add_node(component=reranker, name="Reranker", inputs=["Retriever"])
-    p.add_node(component=reader, name="Reader", inputs=["Reranker"])
+    if args.generative:
+        p.add_node(component=reranker, name="Reranker", inputs=["Retriever"])
+        p.add_node(component=reader, name="Reader", inputs=["Reranker"])
 
     # store top 3 retrieved docs
     results = dict()
     for i in tqdm(range(len(queries))):
         res = p.run(query=queries[1][i])
-        #results = jsonify(res, reader=False)
-        results[i] = jsonify(res)
+        results[i] = jsonify(res, reader=args.generative)
     
     # output results into json file
     json_output = json.dumps(results, indent=4) 
-    with open("data/effective/res_t5.json", "w") as outfile:
+    with open("data/effective/res.json", "w") as outfile:
         outfile.write(json_output)
     
 
